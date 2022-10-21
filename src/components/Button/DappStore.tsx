311d898d1b4004553e3200182d92d6f527d53aee
Janek <janek.rahrt@me.com>
2022-10-21 15:22:01 +0200

    fix: lightmode

--- src/components/Button/DappStore.tsx
+++ src/components/Button/DappStore.tsx
@@ -8,7 +8,7 @@ export const DappStoreButton: FC<{
   return (
     <a
       href={href}
-      className="flex flex-row items-start px-5 py-4 gap-2 bg-black border border-solid border-white rounded-lg min-w-[215px] h-[72px] overflow-hidden"
+      className="flex flex-row items-start px-5 py-4 gap-2 bg-black text-white border border-solid border-white rounded-lg min-w-[215px] h-[72px] overflow-hidden"
     >
       <img src={iconUrl} alt={name} className="w-10 h-10 rounded-full" />
       <div>
